bc0434886f1560ee1a0be13059fb67e315e4ddaa
Philippe <villareal.philippe@gmail.com>
2024-04-09 14:35:36 -0700

    added some TODOs

--- gutenberg-rdf-parser.php
+++ gutenberg-rdf-parser.php
@@ -4,9 +4,14 @@ include "./vendor/autoload.php";
 
 use Gutenberg\Parser\GutenbergRDFParser;
 
-$ignored = [56057]; // These files are not parsable at the moment TODO: gracefully catch this and note all files that fail.
+//TODO: gracefully catch this and note all files that fail.
+$ignored = [56057]; // These files are not parsable at the moment
+
 $folders = explode(PHP_EOL, shell_exec('ls -1 "$(pwd)/cache/epub"'));
+
+//TODO: I want to remove this, one possible solution would be to rewrite the loops in bash so as to free up memory per loop.
 ini_set('memory_limit', '512M');
+
 $parser = new GutenbergRDFParser();
 $batchSize = 5000;
 $batchNumber = 1;
