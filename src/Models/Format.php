535a9afeb0dab2a90cc28dbf67197dc839a128a3
Philippe <villareal.philippe@gmail.com>
2024-04-10 13:00:05 -0700

    minor update, removed AllowDynamicProperties

--- src/Models/Format.php
+++ src/Models/Format.php
@@ -5,12 +5,13 @@ namespace Gutenberg\Models;
 use AllowDynamicProperties;
 use Gutenberg\Traits\JsonSerialize;
 
-#[AllowDynamicProperties] class Format implements \JsonSerializable
+class Format implements \JsonSerializable
 {
     
     public string $fileUrl;
     public string $isFormatOf;
     public string $modifiedDate;
+    public string $httpHeaderFormat;
 
     use JsonSerialize;
 }
